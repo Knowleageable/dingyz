8358a38104e9e61eb911e1db31398b4994f30d79
dingyz <dingyz@taikanglife.com>
2017-08-24 20:22:54 +0800

    dao sql and frame

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,11 @@
+package org.seckill.dao;
+
+/**
+ * Created by dingyz on 2017/8/23.
+ *
+ * @Description:
+ * @Modified By:
+ */
+public class SeckillDao {
+
+}
---
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,10 @@
+package org.seckill.dao;
+
+/**
+ * Created by dingyz on 2017/8/23.
+ *
+ * @Description:
+ * @Modified By:
+ */
+public class SuccessKilledDao {
+}
